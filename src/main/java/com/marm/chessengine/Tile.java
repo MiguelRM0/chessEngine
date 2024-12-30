58668e7122069b2bccf2ccc6c5398e4a3db451ce
Miguel Romero <mrm040@bucknell.edu>
2024-12-30 16:28:36 -0600

    testing

--- src/main/java/com/marm/chessengine/Tile.java
+++ src/main/java/com/marm/chessengine/Tile.java
@@ -1,18 +1,4 @@
-/******************************************
- *CSCI 205 - Software Engineering and Design
- *Spring 2024
- *Instructor: Prof. Lily Romano / Prof. Joshua Stough
- *
- *Name: Miguel Romero
- *Section: 01
- *Date: 12/30/24
- *Time: 4:11 PM
- *
- *Project: chessEngine
- *Package: com.marm.chessengine
- *Class: Tile
- *Description:
- * **************************************** */
+
 package com.marm.chessengine;
 
 public class Tile {
